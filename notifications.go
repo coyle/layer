325f0c003afb8b4c78a73b7aa6783c15c8219f19
Coyle <dennis@coyle.io>
2016-03-18 13:07:40 -0400

    added notification file

---
+++ notifications.go
@@ -0,0 +1,7 @@
+package layer
+
+// SetUsersBadge sets an external unread count for a particular user
+func SetUsersBadge() {}
+
+// GetUsersBadge reads the badge for a particular user
+func GetUsersBadge() {}
